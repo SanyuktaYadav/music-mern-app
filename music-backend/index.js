cf23791bd9514d2e89b12108e5872701bf01f2cf
Sanyukta Yadav <56250040+SanyuktaYadav@users.noreply.github.com>
2025-07-29 21:30:30 +0000

    dotenv package added in backend and enabled port from env file also

--- music-backend/index.js
+++ music-backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 1000;
+const dotenv = require('dotenv');
+
+dotenv.config();
+const port = process.env.PORT || 3000;
 
 app.get("/hello", (req, res) => {
   res.send("hello");
