0951dd819a4a73bd5e29eb0267a9887410540535
Sanyukta Yadav <56250040+SanyuktaYadav@users.noreply.github.com>
2025-09-04 18:43:05 +0000

    Some UI update in navbar

--- music-frontend/src/components/NavBar.jsx
+++ music-frontend/src/components/NavBar.jsx
@@ -30,16 +30,18 @@ const NavBar = () => {
     return (
         <nav className="fixed top-0 left-0 w-full flex bg-slate-800 justify-between z-50">
             <div className="flex gap-2 p-4 items-center">
-                <Link to="/" className="flex items-center space-x-3 text-gray-300 hover:text-white">
+                <Link to="/" className="flex items-center space-x-3 text-gray-300 hover:text-white pr-1">
                     <div className="bg-gray-400 text-black rounded-xl flex items-center justify-center text-xl font-bold">
                         🎵
                     </div>
                     <span className="text-white text-lg font-semibold">MyMusic</span>
                 </Link>
-                {isAdmin && <Link to="/AddSong" className="px-4 text-gray-300 cursor-pointer">Add Song</Link>}
+                {isAdmin && <Link to="/AddSong" className="px-1 text-gray-300 cursor-pointer font-bold">Add Song</Link>}
+                {isAdmin && <Link to="/Users" className="px-1 text-gray-300 cursor-pointer font-bold">Users</Link>}
             </div>
 
             <div ref={dropdownRef} className="relative inline-block text-left">
+                {!isLoggedIn && <Link onClick={() => { navigate("/Login") }} className="px-1 text-gray-300 cursor-pointer font-bold">Login</Link>}
 
                 {/* Trigger Button */}
                 <button
@@ -53,7 +55,6 @@ const NavBar = () => {
                 {isOpen && (
                     <div className="absolute mt-2 right-0 w-40 bg-white border border-gray-300 rounded-md shadow-lg z-50">
                         <ul className="py-1 text-gray-700">
-                            {!isLoggedIn && <li onClick={() => { navigate("/Login") }} className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Login</li>}
                             {isLoggedIn && <li
                                 onClick={async () => {
                                     const success = await logout();
