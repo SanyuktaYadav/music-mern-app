bcbf024fe6825d8213f0e0eeeb8d9b6ebf8c4361
Sanyukta Yadav <56250040+SanyuktaYadav@users.noreply.github.com>
2025-09-06 21:47:07 +0000

    BASE_URL updated for respective hostname

--- music-frontend/src/utils/constants.js
+++ music-frontend/src/utils/constants.js
@@ -1,1 +1,6 @@
-export const BASE_URL = "https://fluffy-space-cod-pwgxwq5gq4vc7w7p-3000.app.github.dev";
+export const BASE_URL =
+  location.hostname === "fluffy-space-cod-pwgxwq5gq4vc7w7p-5173.app.github.dev"
+    ? "https://fluffy-space-cod-pwgxwq5gq4vc7w7p-3000.app.github.dev"
+    : location.hostname === "localhost"
+      ? "http://localhost:3000"
+      : "https://music-backend-wy5e.onrender.com";
