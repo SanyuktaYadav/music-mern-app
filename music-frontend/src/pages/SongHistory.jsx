038fd306a82cf7f287087c48299bd5afa79e20dd
Sanyukta Yadav <56250040+SanyuktaYadav@users.noreply.github.com>
2025-09-10 12:58:59 +0000

    SongHistory spinner added for loading state

--- music-frontend/src/pages/SongHistory.jsx
+++ music-frontend/src/pages/SongHistory.jsx
@@ -1,21 +1,27 @@
+import moment from 'moment';
 import { useEffect, useState } from "react";
 import { useSelector } from 'react-redux';
-import { fetchSongHistory } from "../actions/songHistoryActions.js";
 import { useNavigate } from "react-router";
-import moment from 'moment';
+import { fetchSongHistory } from "../actions/songHistoryActions.js";
+import Spinner from "../components/Spinner.jsx";
 
 const SongHistory = () => {
     const currentUser = useSelector(state => state.currentUser.user);
     const navigate = useNavigate();
     const [songHistory, setSongHistory] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     const isLoggedIn = !!currentUser;
 
     useEffect(() => {
         const fetchData = async () => {
             if (isLoggedIn) {
+                setIsLoading(true);
                 const response = await fetchSongHistory();
-                setSongHistory(response?.data?.history);
+                if (response.status === 200) {
+                    setSongHistory(response?.data?.history);
+                }
+                setIsLoading(false);
             }
         }
         fetchData();
@@ -35,46 +41,48 @@ const SongHistory = () => {
                 </div>
             </div>
 
-            <div className="flex flex-wrap mt-8 justify-center w-full">
-                {songHistory.map((item) => {
-                    const song = item?.song;
+            {isLoading
+                ? <Spinner />
+                : <div className="flex flex-col mt-8 justify-center items-center gap-2">
+                    {songHistory.map((item) => {
+                        const song = item?.song;
 
-                    return (
-                        <div
-                            key={item._id}
-                            className={`w-full mx-20 my-4 p-4 rounded-2xl border-slate-500 ${song ? 'cursor-pointer' : ''} bg-gradient-to-b from-slate-100 to-white/80 flex justify-start flex-wrap shadow-md`}
-                            onClick={() => { song ? navigate("/Song/" + item.song._id) : null }}
-                        >
-                            {song &&
-                                <img src={song.songPoster} alt="song" className="w-24 h-24 object-cover mx-12" />
-                            }
-
-                            <div className="my-2 flex flex-1 justify-between items-center">
+                        return (
+                            <div
+                                key={item._id}
+                                className={`w-[70%] rounded-2xl border-slate-500 ${song ? 'cursor-pointer' : ''} bg-gradient-to-b from-slate-100 to-white/80 flex justify-start shadow-md`}
+                                onClick={() => { song ? navigate("/Song/" + item.song._id) : null }}
+                            >
                                 {song &&
-                                    (<div className="flex flex-col">
-                                        <div className="text-xl font-semibold text-slate-600 text-base">
-                                            {song.songName}
-                                        </div>
-                                        From: {song.albumName}
-                                    </div>)
+                                    <img src={song.songPoster} alt="song" className="w-24 h-24 object-cover p-4" />
                                 }
-                                {!song &&
-                                    (<div>
-                                        <div className="mx-12 text-xl font-semibold text-slate-600">
-                                            {item.songName + " song deleted"}
-                                        </div>
-                                    </div>)
-                                }
-                                <div>
-                                    {formatDate(item?.playedAt)}
+
+                                <div className="flex flex-1 justify-between items-center gap-1">
+                                    {song &&
+                                        (<div className="flex flex-col">
+                                            <div className="text-xl font-semibold text-slate-600 text-base">
+                                                {song.songName}
+                                            </div>
+                                            From: {song.albumName}
+                                        </div>)
+                                    }
+                                    {!song &&
+                                        (<div>
+                                            <div className="mx-12 p-4 text-xl font-semibold text-slate-600">
+                                                {item.songName + " song deleted"}
+                                            </div>
+                                        </div>)
+                                    }
+                                    <div className='p-4'>
+                                        {formatDate(item?.playedAt)}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })}
-            </div>
+                        )
+                    })}
+                </div>
+            }
         </div>
-
     )
 }
 
